f3a75e9e3f273e32654e1c3e859bf0617d1fc35a
Philip Boen <philipboen001@gmail.com>
2024-12-18 22:21:59 +0300

    update embedding client to use Azure endpoint and GitHub token

--- utils/create_embedding.py
+++ utils/create_embedding.py
@@ -1,12 +1,17 @@
-
 import os
 from typing import List
 from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+token = os.environ["GITHUB_TOKEN"]
+endpoint = "https://models.inference.ai.azure.com"
+model_name = "text-embedding-3-small"
+
+client = OpenAI(base_url=endpoint, api_key=token)
+
 
 def create_embedding(text: str) -> List[float]:
-    response = client.embeddings.create(model="text-embedding-3-small", input=text)
+    response = client.embeddings.create(model=model_name, input=text)
     return response.data[0].embedding
